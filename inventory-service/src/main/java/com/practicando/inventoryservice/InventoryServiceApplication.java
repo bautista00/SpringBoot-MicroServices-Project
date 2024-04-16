444597386c7d54bb1d457e709cf022eaf9c65d97
bautista00 <bautistaquesada00@gmail.com>
2024-04-16 15:52:12 -0300

    Eureka Server & Client connected

--- inventory-service/src/main/java/com/practicando/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/practicando/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,13 @@ import com.practicando.inventoryservice.Repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
