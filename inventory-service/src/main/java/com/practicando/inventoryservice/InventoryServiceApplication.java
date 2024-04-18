8cfae40b0988e20a3a53203db50e2f4985ab4142
bautista00 <bautistaquesada00@gmail.com>
2024-04-18 15:39:56 -0300

    [Fix] Communication between server & client

--- inventory-service/src/main/java/com/practicando/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/practicando/inventoryservice/InventoryServiceApplication.java
@@ -7,11 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
