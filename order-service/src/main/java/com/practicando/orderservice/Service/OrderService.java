916e819481348bbd417217a7930175c916f64449
bautista00 <bautistaquesada00@gmail.com>
2024-04-18 16:05:17 -0300

    [Update] Client side LoadBalancing

--- order-service/src/main/java/com/practicando/orderservice/Service/OrderService.java
+++ order-service/src/main/java/com/practicando/orderservice/Service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
 
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -42,8 +42,8 @@ public class OrderService {
 
         //Llamado al inventory service y capturar una orden si el
         // producto esta en stock
-        InventoryResponse[] inventoryResponsArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
