1e97fa0b4b5a7b1654c50ba39411d9dba0d7d4b4
bautista00 <bautistaquesada00@gmail.com>
2024-05-01 16:51:10 -0300

    [Production-Fix] Server is up & running again :)

--- discovery-server/src/main/java/com/practicando/discoveryserver/SecurityConfig.java
+++ discovery-server/src/main/java/com/practicando/discoveryserver/SecurityConfig.java
@@ -1,6 +1,5 @@
 package Config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +7,9 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf()
-                .ignoringRequestMatchers("/eureka/**");
+        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
         return httpSecurity.build();
     }
 }
